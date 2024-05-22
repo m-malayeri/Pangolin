2a54b3defd56468f44cb3d73728a63a2435fea0c
h-brs <student@h-brs.de>
2024-05-22 15:05:29 +0000

    compatible with ubuntu 20.04

--- include/pangolin/video/drivers/ffmpeg.h
+++ include/pangolin/video/drivers/ffmpeg.h
@@ -28,6 +28,10 @@
 #ifndef PANGOLIN_FFMPEG_H
 #define PANGOLIN_FFMPEG_H
 
+#define AV_CODEC_FLAG_GLOBAL_HEADER (1 << 22)
+#define CODEC_FLAG_GLOBAL_HEADER AV_CODEC_FLAG_GLOBAL_HEADER
+#define AVFMT_RAWPICTURE 0x0020
+
 #include <pangolin/pangolin.h>
 #include <pangolin/video/video.h>
 
